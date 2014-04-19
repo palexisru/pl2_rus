2fb2b4dc2c033d68c92b356cf380ae428626e2d3
palexisru <pl2-rainbow@mail.ru>
2014-04-19 12:25:43 +0400

    Add: Common PL/2 library and documentation

---
+++ v01/pl2/pl2/locomotive/front/lexical/parser/sequence/sequence.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pl2.locomotive.front.lexical.parser.sequence
+    {
+    class Sequence
+        {
+        }
+    }
