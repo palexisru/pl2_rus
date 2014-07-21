0237d3883addc0e95f888bcabf419790f6bf1ff8
palexisru <pl2-rainbow@mail.ru>
2014-07-21 22:19:58 +0400

    translation tree review

---
+++ v01/pl2/pl2/proxy/environment/environment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pl2.proxy.environment
+{
+    /// <summary>
+    /// сохранение информации об окружении, не входящему в состав текущего проекта
+    /// </summary>
+    class Environment
+    {
+    }
+}
