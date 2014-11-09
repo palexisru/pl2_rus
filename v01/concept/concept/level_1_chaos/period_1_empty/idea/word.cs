a1f587f2cf2bb328540167eacdb6666a4680e5ca
palexisru <pl2-rainbow@mail.ru>
2014-11-09 16:55:29 +0300

    concept directory and namespace tree changed

--- v01/concept/concept/level_1_chaos/period_1_empty/idea/word.cs
+++ v01/concept/concept/level_1_chaos/period_1_empty/idea/word.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace pl2.concept.level_01.nature
+namespace pl2.concept.level_1_chaos.period_1_empty.idea
 {
-    class Empty
+    interface Word
     {
     }
 }
--- v01/concept/concept/level_1_chaos/period_1_empty/nature/empty.cs
+++ v01/concept/concept/level_1_chaos/period_1_empty/nature/empty.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace pl2.concept.level_01.idea
+namespace pl2.concept.level_1_chaos.period_1_empty.nature
 {
-    class Word
+    class Empty
     {
     }
 }
