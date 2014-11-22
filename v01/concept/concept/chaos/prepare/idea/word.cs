8dba9940f75719985927da195b944e560f828fcf
palexisru <pl2-rainbow@mail.ru>
2014-11-22 12:15:01 +0300

    deleted numbers from names

--- v01/concept/concept/chaos/prepare/idea/word.cs
+++ v01/concept/concept/chaos/prepare/idea/word.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace pl2.concept.level_1_chaos.period_1_empty.idea
+namespace pl2.concept.chaos.prepare.idea
 {
     interface Word
     {
